ac6f75e31a58938bb8f53f428fe838e8b96f9c44
Tanish Hupare <huparetanish@gmail.com>
2024-05-25 19:36:34 -0700

    skybox asset

--- Assets/Scenes/PlayerMovement.cs
+++ Assets/Scenes/PlayerMovement.cs
@@ -2,21 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5.0f;
     public float mouseSensitivity = 100.0f;
 
+    private Rigidbody rb;
     private float verticalLookRotation = 0.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true; // Prevent Rigidbody from rotating with physics forces
+    }
 
     void Update()
     {
-        // Player movement
-        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
-        float strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-
-        transform.Translate(strafe, 0, translation);
-
         // Mouse look
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
@@ -27,5 +29,15 @@ public class PlayerMovement : MonoBehaviour
         verticalLookRotation = Mathf.Clamp(verticalLookRotation, -90f, 90f);
 
         Camera.main.transform.localRotation = Quaternion.Euler(verticalLookRotation, 0, 0);
+    }
+
+    void FixedUpdate()
+    {
+        // Player movement
+        float translation = Input.GetAxis("Vertical") * speed * Time.fixedDeltaTime;
+        float strafe = Input.GetAxis("Horizontal") * speed * Time.fixedDeltaTime;
+
+        Vector3 move = transform.right * strafe + transform.forward * translation;
+        rb.MovePosition(rb.position + move);
     }
 }
