ef792127a6405766cced219a49726662103fff87
cshafizadeh <shafizac@oregonstate.edu>
2024-05-28 17:05:59 -0700

    feat: finished

--- Assets/scripts/SceneSwitcher.cs
+++ Assets/scripts/SceneSwitcher.cs
@@ -5,10 +5,16 @@ public class SceneSwitcher : MonoBehaviour
 {
     // Name of the game scene
     public string gameSceneName = "GameScene"; // Replace with the name of your game scene
+    public string homeSceneName = "Startup";
 
     // Method to be called when the start button is clicked
     public void StartGame()
     {
         SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void HomeScreen()
+    {
+        SceneManager.LoadScene(homeSceneName);
     }
 }
